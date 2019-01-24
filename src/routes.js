af44d649eeea15804814e7369dbc3cd4c06799aa
schmartmann <stefanhartmann@gmail.com>
2019-01-24 14:40:54 -0500

    add pry for debugging

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { json as parseJsonBody } from 'body-parser';
-
+import pry from 'pryjs'
 import { register as registerMeasurements } from './measurements/measurements-routes';
 import { register as registerStats } from './statistics/stats-routes';
 
@@ -12,7 +12,11 @@ server.use(parseJsonBody());
 // dummy handler so you can tell if the server is running
 // features/01-measurements/03-update-measurement.feature
 // e.g. `curl localhost:8000`
-server.get('/', (req, res) => res.send('Weather tracker is up and running!\n'));
+server.get( '/',
+  ( req, res ) => {
+    res.send( 'Weather tracker is up and running!\n' );
+  }
+);
 
 registerMeasurements(server);
 registerStats(server);
